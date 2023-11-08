423f1d5dfa426085e44b4936651c873a7fa1ed14
Andrei <coursestud@mail.ru>
2023-11-08 15:16:43 +0300

    feat: add expires cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { BadRequestException, Body, Controller, Get, HttpCode, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
@@ -9,7 +19,10 @@ import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService, private readonly configService: ConfigService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('register')
   signUp(@Body() createUserDto: CreateUserDto) {
@@ -18,12 +31,12 @@ export class AuthController {
 
   @Post('login')
   async signIn(@Body() loginUserDto: LoginDto, @Res() response: Response) {
-    const tokens = await this.authService.signIn(loginUserDto)
-    if(tokens) {
-      this.setTokensToCookie(tokens, response)
+    const tokens = await this.authService.signIn(loginUserDto);
+    if (tokens) {
+      this.setTokensToCookie(tokens, response);
       return;
     }
-    throw new BadRequestException('Cant login!')
+    throw new BadRequestException('Cant login!');
   }
 
   @UseGuards(AccessTokenGuard)
@@ -33,14 +46,24 @@ export class AuthController {
     return this.authService.logout(req.user.id);
   }
 
-  private setTokensToCookie(tokens: {accessToken: string, refreshToken: string}, res: Response) {
-    res.cookie(COOKIE.ACCESS, tokens.accessToken,{
+  private setTokensToCookie(
+    tokens: { accessToken: string; refreshToken: string },
+    res: Response,
+  ) {
+    res.cookie(COOKIE.ACCESS, tokens.accessToken, {
       httpOnly: true,
       sameSite: 'lax',
-      expires: new Date(this.configService.get('JWT_ACCESS_EXP')),
+      // expires: new Date(Date.now() + 2000),
+      maxAge: 1 * 60 * 60 * 1000,
       path: '/',
-  })
-    res.cookie(COOKIE.REFRESH, tokens.refreshToken)
-    res.status(HttpStatus.CREATED).json({...tokens})
+    });
+    res.cookie(COOKIE.REFRESH, tokens.refreshToken, {
+      httpOnly: true,
+      sameSite: 'lax',
+      // expires: new Date(Date.now() + 2000),
+      maxAge: 2 * 60 * 60 * 1000,
+      path: '/',
+    });
+    res.status(HttpStatus.CREATED).json({ ...tokens });
   }
 }
